094de80acfcb8b08e7ffe7cb5e60f5cc7807d357
Ramakrishna2624 <ramakrishnagadipally@gmail.com>
2026-08-20 14:35:19 +0530

    deploy: add pre-built dist for Render

---
+++ backend/dist/src/common/pipes/validation.pipe.d.ts
@@ -0,0 +1,2 @@
+import { ValidationPipe as NestValidationPipe, ValidationPipeOptions } from '@nestjs/common';
+export declare const createValidationPipe: (options?: ValidationPipeOptions) => NestValidationPipe;
